f7c46f0136fd038f10b1ea166adf31b457c1088d
ACER <ACER@NAM-LAPTOP>
2023-12-21 09:35:49 +0700

    Add project files.

---
+++ AVL_Balance_BST_22h1120016/AVL_Balance_BST_22h1120016.cpp
@@ -0,0 +1,40 @@
+#include "AVL.h"
+#include<iostream>
+using namespace std;
+
+int main() {
+    AVL* tree = new AVL();
+    Node* n;
+    n = new Node(50);
+    tree->InsertNode(n);
+    n = new Node(20);
+    tree->InsertNode(n);
+    n = new Node(30);
+    tree->InsertNode(n);
+    n = new Node(10);
+    tree->InsertNode(n);
+    n = new Node(-5);
+    tree->InsertNode(n);
+    n = new Node(7);
+    tree->InsertNode(n);
+    n = new Node(15);
+    tree->InsertNode(n);
+    n = new Node(35);
+    tree->InsertNode(n);
+    n = new Node(57);
+    tree->InsertNode(n);
+    n = new Node(65);
+    tree->InsertNode(n);
+    n = new Node(55);
+    tree->InsertNode(n);
+    n = new Node(-1);
+    tree->InsertNode(n);
+ 
+    tree->deleteNode(35);
+    tree->deleteNode(55);
+    tree->deleteNode(20);
+    tree->deleteNode(15);
+    tree->TravelLRN();
+
+    tree->deleteAll();
+}
